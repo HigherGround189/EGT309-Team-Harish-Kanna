44b4b037b0b99cfe751780cedc7149bf270e731a
HigherGround189 <thehigherground18@gmail.com>
2025-12-01 16:44:18 +0800

    frontend querying Flask route sucess!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,30 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div v-for="(file_paths, model) in models" :key="model">
+    <h1>{{ model }}</h1>
+    <div v-for="file in file_paths" :key="file">
+      <p>{{ file }}</p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+  export default {
+    data() {
+      return {
+        models: {}
+      }
+    },
+    mounted() {
+    // Fetch model data from flask
+      fetch("/api/models")
+      .then(res => res.json())
+      .then(data => {
+        this.models = data;
+      });
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
