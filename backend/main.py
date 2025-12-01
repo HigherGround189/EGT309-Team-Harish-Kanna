fd0dab3eee63b46c8c09d0fba916baf7891abbee
HigherGround189 <thehigherground18@gmail.com>
2025-12-01 16:56:58 +0800

    added dynamic file server in flask

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, send_from_directory, abort
+from flask import Flask, jsonify, send_file, abort
 from pathlib import Path
 
 app = Flask(__name__, static_folder="dist")
 
 # Directory containing saved models
-MODEL_DIR = Path(__file__).parent / "saved_models"
+SAVED_MODEL_DIR = Path(__file__).parent / "saved_models"
 
-# List all models in MODEL_DIR
+# List all models in SAVED_MODEL_DIR
 @app.route("/api/models")
 def list_models():
     models = {}
@@ -16,7 +16,7 @@ def list_models():
     # ├── RandomForestClassifier/
     # └── XGBoostClassifier/
     # In this case, the following loop would iterate through "RandomForestClassifier/" and "XGBoostClassifier/"
-    for model_folder in MODEL_DIR.iterdir():
+    for model_folder in SAVED_MODEL_DIR.iterdir():
         # Skip file if not directory (shouldn't happen, but good to have in case) 
         if not model_folder.is_dir():
             continue
@@ -32,6 +32,24 @@ def list_models():
 
     return jsonify(models)
 
+# Dyanmic route for serving files (eg: parameters.json)
+@app.route("/api/<model>/<filename>")
+def serve_file(model, filename):
+    # Create path for specific Model (eg: SAVED_MODEL_DIR/XGBoost/)
+    model_dir = SAVED_MODEL_DIR / model
+
+    # Check if the Model directory actually exists 
+    if not model_dir.exists():
+        abort(404, f"Model {model} not found")
+
+    # Create path for specific File being requested (eg: SAVED_MODEL_DIR/XGBoost/parameters.json)
+    file_path = model_dir / filename
+
+    # Check if specific File actually exists
+    if not file_path.exists():
+        abort(404, f"File {filename} not found for model {model}")
+
+    return send_file(file_path)
+
 if __name__ == "__main__":
-    print(MODEL_DIR)
     app.run(debug=True)
