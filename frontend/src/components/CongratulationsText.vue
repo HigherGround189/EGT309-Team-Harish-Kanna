c976f3109fd57b82ee1f9e3602589c291c522e67
HigherGround189 <thehigherground18@gmail.com>
2025-12-04 02:24:20 +0800

    added growth.svg and created CongratulationsText.vue

---
+++ frontend/src/components/CongratulationsText.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="congratulations-text">
+      <img src="@/assets/checkmark.png" class="checkmark" alt=""/>
+      <h2>Training Completed!</h2>
+      <p>Kedro has completed its pipelines. Here are your models' results:</p>
+    </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  .congratulations-text {
+    font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    color: white;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    width: 100%;
+    padding-bottom: 2rem;
+    border-bottom: 2px hsl(0, 0%, 20%) solid;
+  }
+
+  .checkmark {
+    height: 10vh;
+    animation: bounce 1s infinite;
+    margin-bottom: 1rem;
+  }
+
+  @keyframes bounce {
+    0%, 100% {
+      transform: translateY(-25%);
+      animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+    }
+    50% {
+      transform: translateY(0);
+      animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    }
+  }
+
+  .congratulations-text > h2{
+    font-size: 4rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .congratulations-text > p {
+    font-size: 1.2rem;
+  }
+</style>
--- frontend/src/components/ModelInfo.vue
+++ frontend/src/components/ModelInfo.vue
@@ -1,24 +1,27 @@
 <template>
-  <!-- <div v-for="(files, model) in models" :key="model">
+  <div v-for="(files, model) in models" :key="model">
     <h2>{{ model }}</h2>
     <div v-for="(file, filename) in files" :key="filename">
       <p>Api Route: {{ file.url }}</p>
       <img v-if="file.isImage" :src="file.url" :alt="filename" />
-      <pre v-else>{{ file.content }}</pre>
-    </div>
-  </div> -->
-
-  <div class="content-wrapper">
-    <div class="congratulations-text">
-      <img src="@/assets/checkmark.png" class="checkmark" alt=""/>
-      <h2>Training Completed!</h2>
-      <p>Kedro has completed its pipelines. Here are the best results:</p>
+      <pre v-else v-for="line in file.content.split()" :key="line">
+        <p>{{ line }}</p>
+      </pre>
     </div>
   </div>
+
+  <div class="content-wrapper">
+    <CongratulationsText/>
+    <Test/>
+  </div>
+
 </template>
 
 <script>
+import CongratulationsText from './CongratulationsText.vue';
+
   export default {
+    components: { CongratulationsText },
     data() {
       return {
         models: {},
@@ -119,43 +122,7 @@
   }
 
   .content-wrapper {
-    padding: 3rem;
+    padding-block: 3rem;
     min-height: 90vh;
-  }
-
-  .congratulations-text {
-    font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-    color: white;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
-
-  .checkmark {
-    height: 10vh;
-    animation: bounce 1s infinite;
-    margin-bottom: 1rem;
-  }
-
-  @keyframes bounce {
-    0%, 100% {
-      transform: translateY(-25%);
-      animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
-    }
-    50% {
-      transform: translateY(0);
-      animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
-    }
-  }
-
-  .congratulations-text > h2{
-    font-size: 4rem;
-    margin-bottom: 0.5rem;
-  }
-
-  .congratulations-text > p {
-    font-size: 1.2rem;
   }
 </style>
