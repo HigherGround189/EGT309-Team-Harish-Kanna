7468f913c3166e22ef95c914af7e860153adf52d
HigherGround189 <thehigherground18@gmail.com>
2025-12-03 21:09:03 +0800

    Added kedro hook that contacts the visualisation server once all pipelines completes

---
+++ src/egt309_pipeline/hooks.py
@@ -0,0 +1,30 @@
+from kedro.framework.hooks import hook_impl
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
+class TrainingCompleteHook:
+    @hook_impl
+    def after_pipeline_run(self):
+        # The primary url should automatically resolve to the visualisation-server docker container, assuming the pipeline is ran in a container as well.
+        # If the primary url can't be contacted (meaning that the pipeline is running outside a container), the fallback localhost url is used instead.
+        primary_url = "http://visualisation-server:5500/training-complete"
+        fallback_url = "http://localhost:5500/training-complete"
+
+        try:
+            response = requests.get(primary_url, timeout=10)
+            response.raise_for_status() # Raise error if request is unsucessful
+
+            logger.info("Model Evaluation Completed! Sucessfully reloaded frontend!")
+
+        except Exception as exception:
+            logger.error(f"Can't contact visualisation-server container, falling back to localhost instead.\nException: {exception}")
+
+            try:
+                response = requests.get(fallback_url, timeout=10)
+                response.raise_for_status()
+                return response.json()
+            
+            except Exception as exception:
+                logger.error(f"Can't contact Localhost. Is flask server running?\nException: {exception}")
