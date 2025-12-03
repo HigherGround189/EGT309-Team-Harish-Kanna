2433321ea0b6fb3d848ca5fc9e5c7dcc7ec583e3
HigherGround189 <thehigherground18@gmail.com>
2025-12-04 07:00:11 +0800

    changed model training configs

--- src/egt309_pipeline/pipelines/model_training/nodes.py
+++ src/egt309_pipeline/pipelines/model_training/nodes.py
@@ -319,7 +319,7 @@ def train_model(
         estimator=model_to_tune,
         search_spaces=param_grid,
         cv=cv_strategy,
-        scoring="f1",  # F1 is used due to class imbalance
+        scoring="recall_weighted",  # F1 is used due to class imbalance
         n_jobs=-1,
         verbose=0,
         n_iter=options["bayes_search_n_iters"],
