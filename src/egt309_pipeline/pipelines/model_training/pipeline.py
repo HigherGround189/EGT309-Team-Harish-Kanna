a550f58c659f8401f4c0234aebd5cfd41d08190e
HigherGround189 <thehigherground18@gmail.com>
2025-12-03 19:41:12 +0800

    added namespaces to model_evaluation and model_training, and changed default parameters for total iterations searched

--- src/egt309_pipeline/pipelines/model_training/pipeline.py
+++ src/egt309_pipeline/pipelines/model_training/pipeline.py
@@ -52,4 +52,4 @@ def create_pipeline(**kwargs) -> Pipeline:
             )
         )
 
-    return Pipeline(nodes)
+    return Pipeline(namespace="Model Training",prefix_datasets_with_namespace=False, nodes=nodes)
