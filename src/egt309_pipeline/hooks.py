c558a06545068482d2d893d187ac91a5936cf915
HigherGround189 <thehigherground18@gmail.com>
2025-12-07 03:33:08 +0800

    reformatted code and added comments

--- src/egt309_pipeline/hooks.py
+++ src/egt309_pipeline/hooks.py
@@ -12,8 +12,12 @@ console = Console(force_terminal=True, _environ={"COLUMNS": "112"}, color_system
 class DisplayBannerBeforePipelineRuns:
     @hook_impl
     def after_context_created(self):
+        """
+        Prints Banner Text after Kedro Context is created (before pipeline is run)
+        """
+
         # Created with https://www.patorjk.com/software/taag/#p=display&f=ANSI+Shadow&t=EGT309+TEAM%0AHarish+Kanna&x=none&v=4&h=4&w=80&we=false
-        banner_text = """
+        block_banner_text = """
         ███████╗ ██████╗████████╗██████╗  ██████╗  █████╗     ████████╗███████╗ █████╗ ███╗   ███╗
         ██╔════╝██╔════╝╚══██╔══╝╚════██╗██╔═████╗██╔══██╗    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
         █████╗  ██║  ███╗  ██║    █████╔╝██║██╔██║╚██████║       ██║   █████╗  ███████║██╔████╔██║
@@ -29,15 +33,19 @@ class DisplayBannerBeforePipelineRuns:
         ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝  ╚═╝
         """
 
+        pipline_start_alert = "Pipeline Starting..."
+
         # Taken from https://uigradients.com/#SlightOceanView and https://uigradients.com/#Magic
-        gradient_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
+        banner_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
+        text_colours = ["#a8c0ff", "#a17fe0"]
 
         console.print(
-            Gradient(text=banner_text, colors=gradient_colours), justify="center"
+            Gradient(text=block_banner_text, colors=banner_colours),
+            justify="center",
         )
 
         console.print(
-            Gradient(text="Pipeline Starting...", colors=["#a8c0ff", "#a17fe0"]),
+            Gradient(text=pipline_start_alert, colors=text_colours),
             justify="center",
         )
 
@@ -45,6 +53,12 @@ class DisplayBannerBeforePipelineRuns:
 class TrainingCompleteHook:
     @hook_impl
     def after_pipeline_run(self):
+        """
+        Sends a request to the visualisation server's /training-complete route.
+        This is to update the server that the pipeline has completed (aka training is done),
+        so that the server can requery the model metrics to display.
+        """
+
         # The primary url should automatically resolve to the visualisation-server docker container, assuming the pipeline is ran in a container as well.
         # If the primary url can't be contacted (meaning that the pipeline is running outside a container), the fallback localhost url is used instead.
         primary_url = "http://visualisation-server:5500/training-complete"
@@ -56,8 +70,10 @@ class TrainingCompleteHook:
 
             logger.info("Model Evaluation Completed! Sucessfully reloaded frontend!")
 
+            # Prints banner text after pipeline ends (similar to start)
             # Created with https://www.patorjk.com/software/taag/#p=display&f=ANSI+Shadow&t=PIPELINE%0ACOMPLETE&x=none&v=4&h=4&w=80&we=false
-            banner_text = """
+            # Indentation might seem slightly messed up but it will display normally in container logs
+            block_banner_text = """
             ██████╗ ██╗██████╗ ███████╗██╗     ██╗███╗   ██╗███████╗
             ██╔══██╗██║██╔══██╗██╔════╝██║     ██║████╗  ██║██╔════╝
            ██████╔╝██║██████╔╝█████╗  ██║     ██║██╔██╗ ██║█████╗
@@ -79,21 +95,24 @@ class TrainingCompleteHook:
             http://127.0.0.1:5500/
             """
 
-            gradient_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
+            banner_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
+            text_colours = ["#a8c0ff", "#a17fe0"]
 
             console.print(
-                Gradient(text=banner_text, colors=gradient_colours), justify="center"
+                Gradient(text=block_banner_text, colors=banner_colours),
+                justify="center",
             )
 
             console.print(
-                Gradient(
-                    text=visualisation_server_alert, colors=["#a8c0ff", "#a17fe0"]
-                ),
+                Gradient(text=visualisation_server_alert, colors=text_colours),
                 justify="center",
                 style="underline",
             )
 
         except Exception as exception:
+            # If visualisation-server:5500 can't be reached, we can assume that the pipeline is being ran outside a container.
+            # Hence, we will try localhost:5500 instead.
+
             logger.error(
                 f"Can't contact visualisation-server container. Is the pipeline running inside a container? Falling back to localhost instead.\nException: {exception}"
             )
