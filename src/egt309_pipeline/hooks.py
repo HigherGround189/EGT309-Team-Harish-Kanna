b14c0e687620ef9dfdfd68b0c921c8b962fa6437
HigherGround189 <thehigherground18@gmail.com>
2025-12-07 03:05:38 +0800

    added banner to be displayed after pipeline concludes

--- src/egt309_pipeline/hooks.py
+++ src/egt309_pipeline/hooks.py
@@ -33,7 +33,8 @@ class DisplayBannerBeforePipelineRuns:
         gradient_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
 
         console.print(
-            Gradient(text=banner_text, colors=gradient_colours), justify="center"
+            Gradient(text=banner_text, colors=gradient_colours), 
+            justify="center"
         )
 
         console.print(
@@ -55,6 +56,42 @@ class TrainingCompleteHook:
             response.raise_for_status()  # Raise error if request is unsucessful
 
             logger.info("Model Evaluation Completed! Sucessfully reloaded frontend!")
+
+            # Created with https://www.patorjk.com/software/taag/#p=display&f=ANSI+Shadow&t=PIPELINE%0ACOMPLETE&x=none&v=4&h=4&w=80&we=false
+            banner_text = """
+            ██████╗ ██╗██████╗ ███████╗██╗     ██╗███╗   ██╗███████╗
+            ██╔══██╗██║██╔══██╗██╔════╝██║     ██║████╗  ██║██╔════╝
+           ██████╔╝██║██████╔╝█████╗  ██║     ██║██╔██╗ ██║█████╗
+           ██╔═══╝ ██║██╔═══╝ ██╔══╝  ██║     ██║██║╚██╗██║██╔══╝
+            ██║     ██║██║     ███████╗███████╗██║██║ ╚████║███████╗
+            ╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝             
+                                                                                
+              ██████╗ ██████╗ ███╗   ███╗██████╗ ██╗     ███████╗████████╗███████╗
+             ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██║     ██╔════╝╚══██╔══╝██╔════╝
+            ██║     ██║   ██║██╔████╔██║██████╔╝██║     █████╗     ██║   █████╗  
+            ██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝     ██║   ██╔══╝  
+             ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ███████╗███████╗   ██║   ███████╗
+              ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝   ╚═╝   ╚══════╝
+                                                                                                                          
+            """
+
+            visualisation_server_alert = """
+            Visualisation Server running at
+            http://127.0.0.1:5500/
+            """
+
+            gradient_colours = ["#a8c0ff", "#a17fe0", "#3f2b96"]
+
+            console.print(
+                Gradient(text=banner_text, colors=gradient_colours), 
+                justify="center"
+            )
+
+            console.print(
+                Gradient(text=visualisation_server_alert, colors=["#a8c0ff", "#a17fe0"]),
+                justify="center",
+                style="underline"
+            )
 
         except Exception as exception:
             logger.error(
